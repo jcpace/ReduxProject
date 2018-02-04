10886fed55386aa729d8403dbafc2804911878ef
Joshua Pace <joshpace@gmail.com>
2018-02-03 17:48:14 -0800

    add pages detail to BookDetail

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
 export default () => {
   return [
-    {title: 'JS the Hard Parts'},
-    {title: 'Cats'},
-    {title: 'Harry Potter'},
-    {title: 'Eloquent JS'},
+    {title: 'JS the Hard Parts', pages: 101,},
+    {title: 'Cats', pages: 32,},
+    {title: 'Harry Potter', pages: 43,},
+    {title: 'Eloquent JS', pages: 45,},
   ];
 }
