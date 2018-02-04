7659f40221d41e1853fe5d2a6c6d24d6e758dafe
Joshua Pace <joshpace@gmail.com>
2018-02-03 22:02:23 -0800

    notes add

--- src/reducers/reducer_active_book.js
+++ src/reducers/reducer_active_book.js
@@ -1,5 +1,6 @@
 // State argument is not application state, only the state this reducer is responsible for
 // switch statement
+// State is a plain JS object. The Redux state is entirely different from the component state of the React appliation
 export default function(state = null, action) {
   switch(action.type) {
     case 'BOOK_SELECTED' :
